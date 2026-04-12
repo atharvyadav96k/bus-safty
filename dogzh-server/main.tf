626d5181780fe17ace58ae3344195e82313897e6
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-12 19:43:25 +0530

    deploy server

--- dogzh-server/main.tf
+++ dogzh-server/main.tf
@@ -15,6 +15,7 @@ variable "service_name" {
   default     = "dogzh-server"
 }
 
+
 variable "container_image" {
   description = "The URI of the container image in Artifact Registry or GCR"
   type        = string
