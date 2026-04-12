dd41c3e9ab8bd66e0ed4335b17ca90dbe4a2b3f1
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-12 23:39:26 +0530

    resolving the port issues

--- dogzh-server/server/main.go
+++ dogzh-server/server/main.go
@@ -15,14 +15,19 @@ func main() {
 	r := mux.NewRouter()
 
 	controller.RegisterApiRoutes(r)
-	os.Getenv("noss")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "127.0.0.1:8080",
+		Addr:         ":" + port,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
-	fmt.Println("Server is running on http://127.0.0.1:8080")
+	fmt.Printf("Server is running on port %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
