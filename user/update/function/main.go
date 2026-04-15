f1aab320c219b68be0914927d4d0a8420f6e9ae0
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-15 22:58:49 +0530

    ...

--- user/update/function/main.go
+++ user/update/function/main.go
@@ -4,5 +4,5 @@ import "net/http"
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("UpdateUser: User update was  successful."))
+	w.Write([]byte("UpdateUser: User update was successful."))
 }
