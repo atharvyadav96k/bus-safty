7963688a82601e3d882addd21b60f68345f8210c
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-15 22:38:26 +0530

    feat: add cloud functions for user management (create, update, delete)

---
+++ user/get_all_users/function/main.go
@@ -0,0 +1,8 @@
+package org_register
+
+import "net/http"
+
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Organization registration successful"))
+}
