310997969af31e4c2adce0bde74d29bc13aff3aa
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-19 23:01:33 +0530

    register, update buses

--- bus/register/function/main.go
+++ bus/register/function/main.go
@@ -1,8 +1,43 @@
 package org_register
 
-import "net/http"
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	database_models "github.com/atharvyadav96k/bus-safty-app/database/models"
+	"github.com/atharvyadav96k/bus-safty/user/delete/applayer"
+	"github.com/atharvyadav96k/gcp/common/entity"
+	"github.com/atharvyadav96k/gcp/common/req"
+	"github.com/atharvyadav96k/gcp/common/res"
+)
 
 func BusRegister(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("UpdateUser: User update was successful."))
+	if r.Method != http.MethodPost {
+		res.BadRequest(w, []error{errors.New("Method not allowed")})
+	}
+
+	app := applayer.Init()
+	defer app.Close()
+
+	var bus database_models.Vehicle
+	if err := req.ParseBody(r, &bus); err != nil {
+		res.BadRequest(w, []error{err})
+		return
+	}
+
+	if errs := entity.ValidateStruct(&bus); len(errs) != 0 {
+		res.BadRequest(w, errs)
+		return
+	}
+
+	bus.CreatedAt = time.Now()
+	bus.UpdatedAt = time.Now()
+
+	if err := app.StoreCreate(context.Background(), "bus", bus); err != nil {
+		res.BadRequest(w, []error{err})
+		return
+	}
+	res.Created(w, "Bus registered successfully", bus)
 }
--- bus/update/function/main.go
+++ bus/update/function/main.go
@@ -1,8 +1,47 @@
 package org_register
 
-import "net/http"
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	database_models "github.com/atharvyadav96k/bus-safty-app/database/models"
+	"github.com/atharvyadav96k/bus-safty/user/delete/applayer"
+	"github.com/atharvyadav96k/gcp/common/entity"
+	"github.com/atharvyadav96k/gcp/common/req"
+	"github.com/atharvyadav96k/gcp/common/res"
+)
 
 func UpdateBus(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("UpdateUser: User update was successful."))
+	if r.Method != http.MethodPatch {
+		res.BadRequest(w, []error{errors.New("Method not allowed")})
+		return
+	}
+	app := applayer.Init()
+	defer app.Close()
+
+	var bus database_models.Vehicle
+	if err := req.ParseBody(r, &bus); err != nil {
+		res.BadRequest(w, []error{err})
+		return
+	}
+
+	if errs := entity.ValidateStruct(&bus); len(errs) != 0 {
+		res.BadRequest(w, errs)
+		return
+	}
+
+	if err := app.CheckForDuplicate(context.Background(), "org", bus); err != nil {
+		res.BadRequest(w, []error{err})
+		return
+	}
+
+	bus.UpdatedAt = time.Now()
+
+	if err := app.StoreCreate(context.Background(), "bus", bus); err != nil {
+		res.NotFound(w, []error{err})
+		return
+	}
+	res.Success(w, "Org updated successfully", bus)
 }
