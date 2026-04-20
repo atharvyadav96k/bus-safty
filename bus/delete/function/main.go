2cc4de6ce778e8506cf1a98344a69f5b3e35ee61
Atharv Yadav <98886640+atharvyadav96k@users.noreply.github.com>
2026-04-20 23:58:33 +0530

    delete function

--- bus/delete/function/main.go
+++ bus/delete/function/main.go
@@ -1,8 +1,34 @@
 package org_register
 
-import "net/http"
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	database_models "github.com/atharvyadav96k/bus-safty-app/database/models"
+	"github.com/atharvyadav96k/bus-safty/bus/delete/applayer"
+	"github.com/atharvyadav96k/gcp/common/req"
+	"github.com/atharvyadav96k/gcp/common/res"
+)
 
 func DeleteBus(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("UpdateUser: User update was successful."))
+	if r.Method != http.MethodDelete {
+		res.BadRequest(w, []error{errors.New("Method not allowed")})
+		return
+	}
+	app := applayer.Init()
+	defer app.Close()
+
+	var org database_models.Id
+	if err := req.ParseBody(r, &org); err != nil {
+		res.BadRequest(w, []error{err})
+		return
+	}
+
+	if err := app.StoreDelete(context.Background(), "bus", org.ID); err != nil {
+		res.NotFound(w, []error{err})
+		return
+	}
+
+	res.Success(w, "Org deleted successfully", org)
 }
