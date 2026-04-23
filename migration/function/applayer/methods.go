f73e6d85be92ec2e48358327997b34c6c96c51e2
Atharv Yadav <ravi.burman@xperate.com>
2026-04-23 12:52:19 +0530

    refactor(migration): reorder migration tables in GetMigrationTables function

--- migration/function/applayer/methods.go
+++ migration/function/applayer/methods.go
@@ -4,13 +4,15 @@ import database_models "github.com/atharvyadav96k/bus-safty-app/database/models"
 
 func GetMigrationTables() []interface{} {
 	return []interface{}{
-		database_models.Org{},
-		database_models.User{},
-		database_models.Vehicle{},
-		database_models.RFID{},
-		database_models.RootUser{},
-		database_models.User{},
-		database_models.Scanner{},
 		database_models.WhiteListedEmail{},
+		database_models.RFID{},
+
+		database_models.Org{},
+
+		database_models.RootUser{},
+		database_models.Scanner{},
+		database_models.Vehicle{},
+
+		database_models.User{},
 	}
 }
